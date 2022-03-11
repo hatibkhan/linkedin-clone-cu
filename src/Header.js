be62a3ea6efcd721f0b0e1929810b549d36e2737
Shadow Monarch <53879034+oneeyedreaper@users.noreply.github.com>
2022-03-11 21:02:13 +0530

    Fixed LinkedIn Logo

--- src/Header.js
+++ src/Header.js
@@ -8,41 +8,40 @@ import ChatIcon from "@material-ui/icons/Chat";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import HeaderOption from "./HeaderOption";
 import { logout } from "./features/userSlice";
+import logo from "./linkedin.svg";
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
+// import { LinkedIn } from "@material-ui/icons";
 
 const Header = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const logoutHandler = async () => {
-    await auth.signOut();
-    dispatch(logout());
-  };
+	const logoutHandler = async () => {
+		await auth.signOut();
+		dispatch(logout());
+	};
 
-  return (
-    <div className="header">
-      <div className="header__left">
-        <img
-          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
-          alt="Linkedin Logo"
-        />
+	return (
+		<div className="header">
+			<div className="header__left">
+				<img src={logo} alt="Linkedin Logo" />
 
-        <div className="header__search">
-          <SearchIcon />
-          <input type="text" placeholder="Search" />
-        </div>
-      </div>
+				<div className="header__search">
+					<SearchIcon />
+					<input type="text" placeholder="Search" />
+				</div>
+			</div>
 
-      <div className="header__right">
-        <HeaderOption Icon={HomeIcon} title="Home" />
-        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
-        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
-        <HeaderOption Icon={ChatIcon} title="Messaging" />
-        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
-        <HeaderOption title="Me" onClick={logoutHandler} avatar={true} />
-      </div>
-    </div>
-  );
+			<div className="header__right">
+				<HeaderOption Icon={HomeIcon} title="Home" />
+				<HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+				<HeaderOption Icon={ChatIcon} title="Messaging" />
+				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
+				<HeaderOption title="Me" onClick={logoutHandler} avatar={true} />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
